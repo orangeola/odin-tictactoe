14b8b3181b7ddb92db814b96c82e0b70519c306b
orangeola <orangeqmm@gmail.com>
2022-07-15 06:58:17 +0930

    Add grid and grid layout. Add fade in transition for grid

--- script.js
+++ script.js
@@ -14,17 +14,23 @@ const mainMenu = document.getElementById("mainMenu");
 const twoPlayer = document.getElementById("two");
 const onePlayer = document.getElementById("one");
 const banban = document.getElementById("banner");
+const grid = document.getElementById("gridContainer");
 
 twoPlayer.addEventListener('click', optionTwo);
 
 function optionTwo(){
+    setTimeout(function(){ 
+        mainMenu.style.display = "none";
+        grid.style.display = "grid"
+    }, 700);
     mainMenu.classList.add("hidden");
     onePlayer.classList.add("noClick");
     twoPlayer.classList.add("noClick");
     setTimeout(function(){ 
         banban.classList.add("visible");
-        mainMenu.style.display = "none";
-    }, 700);
+        grid.classList.add("visible");
+    }, 720);
+        
 }
 
 onePlayer.addEventListener('click', () => {
