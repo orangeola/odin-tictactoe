b505e6a0d049a6f836132753d6fe7a05a5211e03
orangeola <orangeqmm@gmail.com>
2022-07-15 05:46:45 +0930

    Work on CSS and HTML for main menu. Add banner for grid page that fades in

--- script.js
+++ script.js
@@ -8,5 +8,25 @@ const gameBoard = (() => {
 })();
 
 const Player = () => {
-    
 }
+
+const mainMenu = document.getElementById("mainMenu");
+const twoPlayer = document.getElementById("two");
+const onePlayer = document.getElementById("one");
+const banban = document.getElementById("banner");
+
+twoPlayer.addEventListener('click', optionTwo);
+
+function optionTwo(){
+    mainMenu.classList.add("hidden");
+    onePlayer.classList.add("noClick");
+    twoPlayer.classList.add("noClick");
+    setTimeout(function(){ 
+        banban.classList.add("visible");
+        mainMenu.style.display = "none";
+    }, 700);
+}
+
+onePlayer.addEventListener('click', () => {
+    alert("a");
+})
